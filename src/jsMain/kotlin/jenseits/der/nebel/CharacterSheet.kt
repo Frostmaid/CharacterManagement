911fbc5d1f3f351171f94b149a2fbd551f0ea93f
Swantje <swantje-t@web.de>
2019-12-09 21:49:18 +0100

    move to bulma

--- src/jsMain/kotlin/jenseits/der/nebel/CharacterSheet.kt
+++ src/jsMain/kotlin/jenseits/der/nebel/CharacterSheet.kt
@@ -4,97 +4,104 @@ import kotlinx.html.*
 import org.w3c.dom.HTMLElement
 
 fun TagConsumer<HTMLElement>.createGeneralSection() {
-  form {
-    classes = setOf("pure-form", "pure-form-aligned")
-    fieldSet {
-      legend {
-        +"Allgemeines"
-      }
-      groupWithLabelAndInputField("characterName", "Charaktername")
-      groupWithLabelAndInputField("countryOfOrigin", "Herkunft")
-      groupWithLabelAndInputField("profession", "Beruf")
-      groupWithLabelAndInputField("status", "Status")
-    }
+  h2 {
+    classes = setOf("subtitle")
+    +"Allgemeines"
   }
+
+  div {
+    classes = setOf("container")
+    groupWithLabelAndInputField("characterName", "Charaktername")
+    groupWithLabelAndInputField("countryOfOrigin", "Herkunft")
+    groupWithLabelAndInputField("profession", "Beruf")
+    groupWithLabelAndInputField("status", "Status")
+  }
+
 }
 
 fun TagConsumer<HTMLElement>.createAttributeSection() {
-  form {
-    classes = setOf("pure-form")
-    legend {
-      +"Attribute (Stufe x 20) Start-EP: 920"
+  h2 {
+    classes = setOf("subtitle")
+    +"Attribute (Stufe x 20) Start-EP: 920"
+  }
+  div {
+    classes = setOf("columns")
+    div {
+      classes = setOf("column")
+      attributeCheckbox("magic", "Magie")
+      attributeCheckbox("charisma", "Charisma")
+      attributeCheckbox("intelligence", "Intelligenz")
+      attributeCheckbox("reflexes", "Reflexe")
+      attributeCheckbox("willpower", "Willenskraft")
     }
     div {
-      classes = setOf("pure-g")
-      div {
-        classes = setOf("pure-u-1-2")
-
-        attributeCheckbox("magic", "Magie")
-        attributeCheckbox("charisma", "Charisma")
-        attributeCheckbox("intelligence", "Intelligenz")
-        attributeCheckbox("reflexes", "Reflexe")
-        attributeCheckbox("willpower", "Willenskraft")
-      }
-      div {
-        classes = setOf("pure-u-1-2")
-        attributeCheckbox("strength", "Körperkraft")
-        attributeCheckbox("agility", "Geschicklichkeit")
-        attributeCheckbox("perception", "Wahrnehmung")
-        attributeCheckbox("sleightOfHand", "Fingerfertigkeit")
-        attributeCheckbox("toughness", "Zähigkeit")
-      }
+      classes = setOf("column")
+      attributeCheckbox("strength", "Körperkraft")
+      attributeCheckbox("agility", "Geschicklichkeit")
+      attributeCheckbox("perception", "Wahrnehmung")
+      attributeCheckbox("sleightOfHand", "Fingerfertigkeit")
+      attributeCheckbox("toughness", "Zähigkeit")
     }
   }
+
 }
 
 private fun DIV.attributeCheckbox(id: String, labelName: String) {
   div {
-    classes = setOf("pure-controls")
+    classes = setOf("field is-horizontal")
     label {
-      classes = setOf("pure-checkbox")
+      classes = setOf("checkbox")
       htmlFor = id
       +labelName
 
       input {
         this.id = "${id}1"
         type = InputType.checkBox
-        value = ""
       }
       input {
         this.id = "${id}2"
         type = InputType.checkBox
-        value = ""
       }
       input {
         this.id = "${id}3"
         type = InputType.checkBox
-        value = ""
       }
       input {
         this.id = "${id}4"
         type = InputType.checkBox
-        value = ""
       }
       input {
         this.id = "${id}5"
         type = InputType.checkBox
-        value = ""
       }
     }
   }
 }
 
-private fun FIELDSET.groupWithLabelAndInputField(id: String, labelName: String) {
+private fun DIV.groupWithLabelAndInputField(id: String, labelName: String) {
   div {
-    classes = setOf("pure-control-group")
-    label {
-      htmlFor = id
-      +labelName
+    classes = setOf("field is-horizontal")
+    div {
+      classes = setOf("field-label is-normal")
+      label {
+        htmlFor = id
+        +labelName
+      }
     }
-    input {
-      this.id = id
-      placeholder = labelName
-      type = InputType.text
+    div {
+      classes = setOf("field-body")
+      div {
+        classes = setOf("field")
+        div {
+          classes = setOf("control")
+          input {
+            this.id = id
+            placeholder = labelName
+            type = InputType.text
+          }
+        }
+      }
     }
   }
+
 }
