c1773930c9864763bae26bec0af5b47ad4c18259
Swantje <swantje-t@web.de>
2020-01-29 15:09:53 +0100

    append dynamic an element (wip)

--- frontend/src/main/kotlin/jenseits/der/nebel/charSheet/TalentSection.kt
+++ frontend/src/main/kotlin/jenseits/der/nebel/charSheet/TalentSection.kt
@@ -4,35 +4,46 @@ import jenseits.der.nebel.checkBox
 import jenseits.der.nebel.h2Title
 import jenseits.der.nebel.plusButton
 import kotlinx.html.*
+import kotlinx.html.dom.append
+import org.w3c.dom.HTMLDivElement
 import org.w3c.dom.HTMLElement
+import kotlin.browser.document
 
 fun TagConsumer<HTMLElement>.createTalentSection() {
   h2Title("Talente")
 
-  plusButton { println("foooo") }
+  val containerElement = document.getElementById("talentTable") as HTMLDivElement
 
-  table {
-    classes = setOf("table is-bordered is-striped")
-    thead {
-      tr {
-        th { +"Aktive Talente" }
-        th { +"Bezugsattribute" }
-        th { +"Wert x 15" }
-      }
-    }
-    tbody {
-      tr {
-        td { +"Lorem ipsum + " }
-        td { +"Weisheit" }
-        td {
-          div {
-            classes = setOf("has-text-centered")
-            checkBox("talent1")
-            checkBox("talent2")
-            checkBox("talent3")
+  plusButton {
+
+    containerElement.append {
+      table {
+
+        classes = setOf("table is-bordered is-striped")
+        thead {
+          tr {
+            th { +"Aktive Talente" }
+            th { +"Bezugsattribute" }
+            th { +"Wert x 15" }
+          }
+        }
+        tbody {
+          tr {
+            td { +"Lorem ipsum" }
+            td { +"Weisheit" }
+            td {
+              div {
+                classes = setOf("has-text-centered")
+                checkBox("talent1")
+                checkBox("talent2")
+                checkBox("talent3")
+              }
+            }
           }
         }
       }
     }
   }
+
+
 }
